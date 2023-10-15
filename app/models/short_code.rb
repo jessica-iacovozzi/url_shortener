f0bf9e06b95b6ae0df9f39b800d2c07865a5f01a
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-10-14 21:05:37 -0400

    Added decoding short code with base62

--- app/models/short_code.rb
+++ app/models/short_code.rb
@@ -15,4 +15,16 @@ class ShortCode
 
     result
   end
+
+  def self.decode(string)
+    result = 0
+
+    string.reverse.each_char.with_index do |char, index|
+      power = BASE**index
+      alphabet_index = ALPLHABET.index(char)
+      result += alphabet_index * power
+    end
+
+    result
+  end
 end
